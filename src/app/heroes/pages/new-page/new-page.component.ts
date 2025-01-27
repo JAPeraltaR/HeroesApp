aa30b097567c12d392b64e78800ca7a5fd7467f6
jean peralta <jean.peralta@igmweb.com>
2025-01-27 18:07:57 +0100

    finalizando video 216(SnackBars y redirección)

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroService } from '../../service/hero.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-new-page',
@@ -7,11 +14,78 @@ import { Component } from '@angular/core';
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+
+  public heroForm = new FormGroup({
+    id:  new FormControl< string >(''),
+    superhero:  new FormControl< string >('', { nonNullable: true}),
+    publisher:  new FormControl<Publisher>(Publisher.MarvelComics),
+    alter_ego:  new FormControl<string>(''),
+    first_appearance:  new FormControl<string>(''),
+    characters:  new FormControl<string>(''),
+    alt_img: new FormControl<string>('')
+  });
 
   public publishers = [
     {id:'DC Comics', desc: 'DC - Comics'},
     {id:'Marvel Comics', desc: 'Marvel - Comics'}
   ]
+
+  constructor( private heroService : HeroService,
+               private activatedRoute: ActivatedRoute,
+               private router: Router,
+               private snackBar: MatSnackBar,
+               private dialog: MatDialog ) { }
+
+
+  ngOnInit(): void {
+    if( !this.router.url.includes('edit') ) return;
+    this.activatedRoute.params.pipe(
+      switchMap( ({ id }) => this.heroService.getHeroById( id )),
+    ).subscribe( hero => {
+      if( !hero ) return this.router.navigateByUrl('/');
+      this.heroForm.reset( hero );
+      return;
+    })
+
+  }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(): void {
+    if( this.heroForm.invalid ) return;
+    if( this.currentHero.id ){
+      this.heroService.updateHero( this.currentHero )
+                      .subscribe( hero => {
+                        this.showSnackBar(`${ hero.superhero } Updated!!`)
+                      });
+      return;
+    }
+
+    this.heroService.addHero(this.currentHero)
+                    .subscribe( hero => {
+                      this.router.navigate(['/heroes/edit', hero.id]),
+                      this.showSnackBar(`${ hero.superhero } Created!!`)
+                    });
+  }
+
+  showSnackBar( message: string ): void {
+    this.snackBar.open( message, 'done ', {
+      duration: 2500,    } );
+  }
+
+  onDeleteHero(): void {
+    if( !this.currentHero ) throw Error('Hero id is required');
+    let dialogRef = this.dialog.open(UserProfileComponent, {
+      height: '400px',
+      width: '600px',
+    });
+
+
+  }
+
 
 }
--- src/app/heroes/service/hero.service.ts
+++ src/app/heroes/service/hero.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments.prod';
 
@@ -14,18 +14,34 @@ export class HeroService {
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable< Hero[] > {
-    return this.http.get<Hero[]>(`${ this.baseURl }/heroes`);
+    return this.http.get< Hero[] >(`${ this.baseURl }/heroes`);
   }
 
   getHeroById( id: string ): Observable< Hero | undefined > {
-    return this.http.get<Hero>((`${ this.baseURl }/heroes/${ id }`))
+    return this.http.get< Hero >((`${ this.baseURl }/heroes/${ id }`))
       .pipe(
         catchError( () => of(undefined))
       );
   }
 
-  getSuggestion( query: string ): Observable<Hero[]> {
-    return this.http.get<Hero[]>(`${ this.baseURl }/heroes?q=${ query }&_limit=5`);
+  getSuggestion( query: string ): Observable< Hero[] > {
+    return this.http.get< Hero[] >(`${ this.baseURl }/heroes?q=${ query }&_limit=5`);
+  }
+
+  addHero( hero: Hero ): Observable< Hero > {
+    return this.http.post< Hero >(`${ this.baseURl }/heroes`, hero);
+  }
+
+  updateHero( hero: Hero ): Observable< Hero > {
+    if ( !hero ) throw Error("Hero Id is required");
+    return this.http.patch< Hero >(`${ this.baseURl }/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHeroById( id: string ): Observable< boolean >{
+    return this.http.delete(`${ this.baseURl }/heroes/${ id }`).pipe(
+      catchError(() => of(false)),
+      map( resp => true)
+    )
   }
 
 }
